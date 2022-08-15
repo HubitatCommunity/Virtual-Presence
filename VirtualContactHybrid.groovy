bebd63a1e7ddc66f31b9802341de4c19a5b1fd20
c steele <csteele@pandoradigital.com>
2022-08-15 12:30:18 -0700

    Update VirtualContactHybrid.groovy

--- VirtualContactHybrid.groovy
+++ VirtualContactHybrid.groovy
@@ -16,6 +16,7 @@
  *
  *    Date        Who            What
  *    ----        ---            ----
+ *    2022-08-15  C Steele       Refactored and added Invert & autoOff
  *    2021-06-16  C Steele       Revamped into Contact
  *    2018-07-18  C Steele       Revamped into Presence
  *    2018-07-18  Dan Ogorchock  Original Creation as "Virtual Illuminance Sensor"
@@ -34,54 +35,54 @@ metadata {
 	}
 
       preferences {
-          //standard logging options
-          input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
+		input name: "invert", type: "bool", title: "<b>Logical Invert</b>", description: "<i>On becomes Off, Off becomes On</i>", defaultValue: false
+		input name: "autoOff", type: "enum", title: "<b>Enable Auto-Off (Auto-On)</b>", description: "<i>Automatically turns off (on) the device after selected time.</i>", options: [[0:"Disabled"],[1:"1s"],[2:"2s"],[5:"5s"],[10:"10s"],[20:"20s"],[30:"30s"],[60:"1m"],[120:"2m"],[300:"5m"],[1800:"30m"],[3200:"60m"]], defaultValue: 0
+		//standard logging options
+		input name: "debugOutput", type: "bool", title: "<b>Enable debug logging?</b>", defaultValue: true
+		input name: "descTextEnable", type: "bool", title: "<b>Enable descriptionText logging?</b>", defaultValue: true
       }
 }
 
-def logsOff(){
-    log.warn "debug logging disabled..."
-    device.updateSetting("logEnable",[value:"false",type:"bool"])
+
+void open()		{ invert ? doOff() : doOn() }
+void closed()	{ invert ? doOn() : doOff() }
+void on() 		{ invert ? doOff() : doOn() }
+void off() 		{ invert ? doOn() : doOff() }
+
+void doOn() {
+	sendEvent(name: "contact", value:"open", descriptionText: "contact", unit: "")
+	sendEvent(name: "switch", value:"on", descriptionText: "switch", unit: "")
+	if (autoOff.toInteger()>0){ runIn(autoOff.toInteger(), off) }
+	if (descTextEnable) log.info("On/Open")
 }
 
-def parse(String description) {
+void doOff() {
+	sendEvent(name: "contact", value:"closed", descriptionText: "contact", unit: "")
+	sendEvent(name: "switch", value:"off", descriptionText: "switch", unit: "")
+	if (descTextEnable) log.info("Off/Closed")
+}
+
+
+void parse(String description) {
 	log.debug(${description})
 	def pair = description.split(":")
 	createEvent(name: pair[0].trim(), value: pair[1].trim())
 }
 
+void installed() {}
 
-def open() {
-	sendEvent(name: "contact", value:"open", descriptionText: "contact", unit: "")
-	sendEvent(name: "switch", value:"on", descriptionText: "presence", unit: "")
-	if (logEnable) log.debug("Arrived")
+void configure() {}
+
+void updated() {
+   if (descTextEnable) log.info "Updated()"
+    log.warn "debug logging is: ${debugOutput == true}"
+    log.warn "description logging is: ${descTextEnable == true}"
+    log.warn "invert is: ${invert == true}"
+    if (debugOutput) runIn(1800,logsOff)    
 }
 
-def closed() {
-	sendEvent(name: "contact", value:"closed", descriptionText: "contact", unit: "")
-	sendEvent(name: "switch", value:"off", descriptionText: "presence", unit: "")
-	if (logEnable) log.debug("Departed")
+void logsOff(){
+    log.warn "debug logging disabled..."
+    device.updateSetting("debugOutput",[value:"false",type:"bool"])
 }
 
-def on() {
-	sendEvent(name: "contact", value:"open", descriptionText: "contact", unit: "")
-	sendEvent(name: "switch", value:"on", descriptionText: "presence", unit: "")
-	if (logEnable) log.debug("on")
-}
-
-def off() {
-v	sendEvent(name: "contact", value:"closed", descriptionText: "contact", unit: "")
-	sendEvent(name: "switch", value:"off", descriptionText: "presence", unit: "")
-	if (logEnable) log.debug("off")
-}
-
-def installed(){}
-
-def configure() {}
-
-def updated() {
-    log.trace "Updated()"
-    log.warn "debug logging is: ${logEnable == true}"
-    log.warn "description logging is: ${txtEnable == true}"
-    if (logEnable) runIn(1800,logsOff)    
-}
